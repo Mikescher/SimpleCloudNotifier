0b22a180882f65ba2367e1fd4fa35930bfa2408e
Mike Schwörer <mailport@mikescher.de>
2018-10-21 22:58:26 +0200

    preferences (1)

--- android/app/src/main/java/com/blackforestbytes/simplecloudnotifier/view/SettingsFragment.java
+++ android/app/src/main/java/com/blackforestbytes/simplecloudnotifier/view/SettingsFragment.java
@@ -1,15 +1,29 @@
 package com.blackforestbytes.simplecloudnotifier.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.blackforestbytes.simplecloudnotifier.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat
+public class SettingsFragment extends Fragment
 {
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey)
+    public SettingsFragment()
     {
-        setPreferencesFromResource(R.xml.preferences, rootKey);
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return v;
     }
 }
