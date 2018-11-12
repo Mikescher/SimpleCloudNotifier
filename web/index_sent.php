ae246e92192a13d270a01cc3e34ff644b92b9916
Mike Schwörer <mailport@mikescher.de>
2018-11-12 15:28:07 +0100

    favicon

--- web/index_sent.php
+++ web/index_sent.php
@@ -8,6 +8,8 @@
 	<!--<link rel="stylesheet" href="/css/mini-dark.min.css">-->
 	<link rel="stylesheet" href="/css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/favicon.png"/>
+    <link rel="icon" type="image/png" href="/favicon.ico"/>
 </head>
 <body>
 
