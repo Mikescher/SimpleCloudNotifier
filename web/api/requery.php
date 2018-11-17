21f66d99cdac8bd4962c6cd44307b31acba20b73
Mike Schwörer <mailport@mikescher.de>
2018-11-17 13:44:42 +0100

    fixed timestamp format in requery

--- web/api/requery.php
+++ web/api/requery.php
@@ -41,7 +41,7 @@ foreach ($nonacks_sql as $nack)
 		'title'      => $nack['title'],
 		'body'       => $nack['content'],
 		'priority'   => $nack['priority'],
-		'timestamp'  => $nack['timestamp'],
+		'timestamp'  => strtotime($nack['timestamp']),
 		'usr_msg_id' => $nack['usr_message_id'],
 		'scn_msg_id' => $nack['scn_message_id'],
 	];
