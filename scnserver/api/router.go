686f89f75df1e12b8fdb96e001ba0203c1231169
Mike Schwörer <pubgit@mikescher.com>
2023-06-18 02:22:29 +0200

    change URL to simplecloudnotifier.de

--- scnserver/api/router.go
+++ scnserver/api/router.go
@@ -40,7 +40,7 @@ func NewRouter(app *logic.Application) *Router {
 //	@title			SimpleCloudNotifier API
 //	@version		2.0
 //	@description	API for SCN
-//	@host			scn.blackforestbytes.com
+//	@host			simplecloudnotifier.de
 //
 //	@tag.name		External
 //	@tag.name		API-v1
